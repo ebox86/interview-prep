c8aee5a094a3ee277cfc35b035216ba724209509
Evan Kohout <ebox86@gmail.com>
2023-11-24 08:53:10 -0500

    added some python ds

---
+++ python-ds/lists.py
@@ -0,0 +1,8 @@
+# Creating a list
+fruits = ["apple", "banana", "cherry", "apple"]
+print(len(fruits)) # 4
+# Adding an item
+fruits.append("orange")
+
+print(len(fruits)) # 5
+print(fruits.count("apple")) # 2
---
+++ python-ds/tuple.py
@@ -0,0 +1,3 @@
+# Creating a tuple
+coordinates = (10.0, 20.0)
+print(coordinates[0]) # prints 10.0
